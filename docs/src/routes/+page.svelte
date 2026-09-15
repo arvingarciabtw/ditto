b1a4da57b67c1fc6024784fe11d086f2881942b8
Arvin Garcia <arvin@arvingarcia.com>
2026-09-15 15:09:28 +0800

    feat: minor writing changes

--- docs/src/routes/+page.svelte
+++ docs/src/routes/+page.svelte
@@ -7,28 +7,33 @@
 		<Keyboard />
 	</section>
 
-	<section class="docs">
-		<p class="subtext">
-			see the
-			<a href="/motivation">docs</a>
-			to get started. see
-			<a
-				href="https://github.com/arvingarciabtw/ditto/releases"
-				target="_blank"
-				rel="noopener noreferrer">releases</a
-			>
-			for the installation. see the
-			<a href="/motivation">motivation</a>
-			behind it!
-		</p>
-	</section>
-
 	<section class="description">
 		<p class="subtext">
 			ditto is a cross-platform, system-wide ascii keyboard visualizer and keycaster that mirrors
 			your live keyboard inputs in real time, even when the terminal isn't in focus. it
 			automatically syncs with your native terminal color scheme for seamless, interactive eye
 			candy.
+		</p>
+	</section>
+
+	<section class="docs">
+		<p class="subtext">
+			to get started, see the
+			<a href="/motivation">docs</a>
+			if it fits your use case or if you'd like to mess around and have fun with it.
+		</p>
+	</section>
+
+	<section class="docs">
+		<p class="subtext">
+			see the
+			<a
+				href="https://github.com/arvingarciabtw/ditto/releases"
+				target="_blank"
+				rel="noopener noreferrer">releases</a
+			>
+			for the installation. if you're on linux, there are other
+			<a href="/installation">options</a>.
 		</p>
 	</section>
 </main>
@@ -42,10 +47,16 @@
 	section {
 		max-width: 60rem;
 	}
+	section.keyboard {
+		margin-bottom: 4rem;
+	}
 
 	@media (max-width: 650px) {
 		section.keyboard {
 			display: none;
+		}
+		main {
+			place-content: center;
 		}
 	}
 </style>
